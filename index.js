7523eaa317236ab62abea9429b3e5038a35bdae5
marvinjk6 <marvin.tavares10@gmail.com>
2023-01-20 14:54:15 -0300

    Reducers

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ function orderCake() {
     }
 }
 
+// Reducers: (previousState, action) => newState
+// we need 2 arguments to a reduce:
+// 1º the state of the application before making any change
+// 2º the action
+// the state object might contain more than one property, in that case to handle with this cenario is more safe to create a copy of the state object ...state, with spread operator to not change others properties
+
+const initialState = {
+    numOfCakes: 10,
+    anotherProperty: 0
+}
+
+const reduce = (state = initialState, action) => {
+
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+            }
+        default:
+            return state;
+    }
+
+}
+
+
